c1b02030e840b5ba695e045c512a3dd5b5527104
BallStripz <infinitivea@gmail.com>
2020-12-15 01:26:00 +0700

    Complete Login and Register

---
+++ controllers/user.js
@@ -0,0 +1,49 @@
+const db = require('../models');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const register = async (req, res, next) => {
+  try {
+    const { email, password, name, phone } = req.body;
+    const targetUserByEmail = await db.User.findOne({ where: { email } });
+    const targetUserByPhone = await db.User.findOne({ where: { phone } });
+
+    if (targetUserByEmail || targetUserByPhone) {
+      res.status(400).send({ message: 'Email or phone number has already used.' });
+    } else {
+      const salt = bcrypt.genSaltSync(Number(process.env.SALT_ROUND));
+      const hashedPassword = bcrypt.hashSync(password, salt);
+
+      await db.User.create({
+        email,
+        password: hashedPassword,
+        name,
+        phone,
+      });
+    }
+    res.status(201).send({ message: 'User created.' });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const targetUser = await db.User.findOne({ where: { email } });
+
+  if (targetUser) {
+    if (bcrypt.compareSync(password, targetUser.password)) {
+      const token = jwt.sign({ id: targetUser.id }, process.env.SECRET, { expiresIn: 3600 });
+      res.status(200).send({ token });
+    } else {
+      res.status(400).send({ message: 'Username or password is incorrect.' });
+    }
+  } else {
+    res.status(400).send({ message: 'Username or password is incorrect.' });
+  }
+};
+
+module.exports = {
+  register,
+  login,
+};
--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 require('dotenv').config();
 const db = require('./models');
 const express = require('express');
+const cors = require('cors');
+
+const userRoutes = require('./routes/user');
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/users', userRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running at port ${process.env.PORT}`);
 });
 
-db.sequelize.sync({ force: false }).then(() => {
+db.sequelize.sync({ force: true }).then(() => {
   console.log('Connected Database');
 });
